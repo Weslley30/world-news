b437459cd0876bdf55d9052122fccdc665e41bcd
Weslley Melo Franco <weslleyfrc9@gmail.com>
2026-03-24 17:49:27 -0300

    chore: adding configuration for commits and code standardization

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,26 +1,23 @@
 import { defineConfig, globalIgnores } from "eslint/config";
 import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
+import prettier from "eslint-config-prettier";
 
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
+  prettier,
   {
     rules: {
       "no-alert": "error",
-      "camelcase": "error",
-      "quotes": ["error", "double"],
-      "semi": ["error", "always"],
+      camelcase: "error",
+      quotes: ["error", "double"],
+      semi: ["error", "always"],
       "no-unused-vars": "error",
-      "no-unreachable": "error"
-    }
+      "no-unreachable": "error",
+    },
   },
-  globalIgnores([
-    ".next/**",
-    "out/**",
-    "build/**",
-    "next-env.d.ts",
-  ]),
+  globalIgnores([".next/**", "out/**", "build/**", "next-env.d.ts"]),
 ]);
 
 export default eslintConfig;
